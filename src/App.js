c8b620f3e5d44795168a3c4f94dd14b29a36210e
josh-alhassan <saintjosh26@gmail.com>
2024-03-06 15:40:14 +0100

    add toggle feature

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 const faqs = [
@@ -18,13 +19,42 @@ const faqs = [
 function App() {
   return (
     <div>
-      <Accordion />
+      <Accordion data={faqs} />
     </div>
   );
 }
 
 function Accordion({ data }) {
-  return <p>Hello</p>;
+  return (
+    <div className="accordion">
+      {data.map((el, idx) => (
+        <AccordionItem
+          title={el.title}
+          text={el.text}
+          num={idx}
+          key={el.title}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
+  return (
+    <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
+      <p className="number"> {num < 9 ? `0${num + 1}` : num + 1} </p>
+      <p className="title">{title}</p>
+      <p className="icon"> {isOpen ? "-" : "+"} </p>
+
+      {isOpen && <div className="content-box"> {text} </div>}
+    </div>
+  );
 }
 
 export default App;
